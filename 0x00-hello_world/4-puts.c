07e2ac5886ee2e0d092c95e275baf29b4bd972c5
Oluwatobiloba490 <oluwatobilobafred@gmail.com>
2022-04-10 11:19:20 -0700

    task 4 solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*
-*/
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
-puts("\"Programming is like a multilingual puzzle");
-return(0);
+puts("\"Programming is like a building with a multilingual puzzle");
+return (0);
 }
